bef79d22a3b68e5400adbee84609ed7874db348b
nargok <nargok@gmail.com>
2019-01-13 00:36:07 +0900

    Implement the first web server

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+)
 
 func main() {
-  fmt.Println("Hello from go")
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":4000", nil)
+}
+
+func handler(w  http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello from go")
 }
 
